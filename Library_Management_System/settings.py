4a3996757d84c672c809bb9c857abb7a4aa32db8
Aarav-Rai9 <aarav_rain@yahoo.com>
2024-03-17 11:58:43 +0800

    settings.py

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Library_Management_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': '@Cheeky9bub',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
