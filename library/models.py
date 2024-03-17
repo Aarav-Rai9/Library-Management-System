e35325b860f38b5fa7df418638ea95cbf3c5da43
Aarav-Rai9 <aarav_rain@yahoo.com>
2024-03-17 12:15:38 +0800

    models.py

--- library/models.py
+++ library/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    bookname = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    rating = models.FloatField()
+    cost = models.FloatField()
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    grade = models.CharField(max_length=255)
+    library_card = models.ForeignKey('LibraryCard', on_delete=models.CASCADE)
+
+
+class LibraryCard(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
