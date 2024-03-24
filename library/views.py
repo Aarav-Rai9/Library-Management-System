6e80ff0fd0ec52df11430f46e2e21a831f13a88b
Aarav-Rai9 <aarav_rain@yahoo.com>
2024-03-24 12:53:44 +0800

    views.py

--- library/views.py
+++ library/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
 
-# Create your views here.
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("listCategory")
+    return render(request, "category/addCategory.html")
+
+
+def add_book(request):
+    categories = Category.objects.all()
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, "category/addBook.html", {"category": categories})
+
+
+def list_category(request):
+    category = Category.objects.all()
+    return render(request, "category/listCategory.html", {"categories": category})
