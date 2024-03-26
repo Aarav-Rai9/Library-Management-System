cbfc41d5f0ee3d689bb65e9b4a7eb895a78e1a8f
Aarav-Rai9 <aarav_rain@yahoo.com>
2024-03-26 20:49:09 +0800

    views.py

--- library/views.py
+++ library/views.py
@@ -17,9 +17,15 @@ def add_book(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect("listBook")
     return render(request, "book/addBook.html", {"category": categories})
 
 
 def list_category(request):
     category = Category.objects.all()
     return render(request, "category/listCategory.html", {"categories": category})
+
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "book/listBook.html", {"books": books})
